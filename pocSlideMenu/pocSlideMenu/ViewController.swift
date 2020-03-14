e8a708371731a0c9dab147a6165fc82c0ee6ef00
Vanoshan Ramdhani <vanoshan1234@gmail.com>
2020-03-14 12:27:39 +0200

    Added files via upload

---
+++ pocSlideMenu/pocSlideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pocSlideMenu
+//
+//  Created by Vanoshan  Ramdhani on 2020/03/14.
+//  Copyright © 2020 Vanoshan  Ramdhani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
